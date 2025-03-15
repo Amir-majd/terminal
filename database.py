08580fb3c95ba617b0b9b405ccb9f9fcd9f05b49
AmirHossein <amirhoseinmaleki59@gmail.com>
2025-03-16 01:39:32 +0330

    fix indentaion in database.db; softcode the database file address

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 import sqlite3
 
+from .config import DATABASE_FILE
 
 def connect_db():
-    conn = sqlite3.connect('database.db')  #
+    conn = sqlite3.connect(DATABASE_FILE)
     return conn
 
-#مدیریت کاربران
-#اقزودن کاربر جدیبد
+
+# مدیریت کاربران
+# اقزودن کاربر جدیبد
 def add_user(username, password, user_type, first_name, last_name, email, phone):
     conn = connect_db()
     cursor = conn.cursor()
@@ -17,66 +19,74 @@ def add_user(username, password, user_type, first_name, last_name, email, phone)
     conn.commit()
     conn.close()
 
-    #دریافت تمام کاربران
-    def get_all_users():
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM Users')
-        users = cursor.fetchall()
-        conn.close()
-        return users
 
-    #دریافت اطلاعات یک کاربر
-    def get_user_by_username(username):
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
-        user = cursor.fetchone()
-        conn.close()
-        return user
+# دریافت تمام کاربران
+def get_all_users():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Users')
+    users = cursor.fetchall()
+    conn.close()
+    return users
 
-    #مدیریت یفر ها
-    #افزودن سفر
-    def add_trip(departure, destination, departure_date, price, status, bus_id, driver_id):
-        conn = connect_db()
-        cursor = conn.cursor()
-        cursor.execute('''
-            INSERT INTO Trips (departure, destination, departure_date, price, status, bus_id, driver_id)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''', (departure, destination, departure_date, price, status, bus_id, driver_id))
-        conn.commit()
-        conn.close()
 
-        #دریافت تمام سفر ها
-        def get_all_trips():
-            conn = connect_db()
-            cursor = conn.cursor()
-            cursor.execute('SELECT * FROM Trips')
-            trips = cursor.fetchall()
-            conn.close()
-            return trips
+# دریافت اطلاعات یک کاربر
+def get_user_by_username(username):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Users WHERE username = ?', (username,))
+    user = cursor.fetchone()
+    conn.close()
+    return user
 
-        #درسافت سفرخاص براساس ایدی کاربر
-        def get_trip_by_id(trip_id):
-            conn = connect_db()
-            cursor = conn.cursor()
-            cursor.execute('SELECT * FROM Trips WHERE trip_id = ?', (trip_id,))
-            trip = cursor.fetchone()
-            conn.close()
-            return trip
-        #مدیدیت اتوبوس ها
-        #اقزودن اتوبوس
-        def add_bus(bus_number, seat_count, model, manufacturer, year, license_plate):
-            conn = connect_db()
-            cursor = conn.cursor()
-            cursor.execute('''
-                INSERT INTO Buses (bus_number, seat_count, model, manufacturer, year, license_plate)
-                VALUES (?, ?, ?, ?, ?, ?)
-            ''', (bus_number, seat_count, model, manufacturer, year, license_plate))
-            conn.commit()
-            conn.close()
 
-#دریافت تمام اتوبوس ها
+# مدیریت یفر ها
+# افزودن سفر
+def add_trip(departure, destination, departure_date, price, status, bus_id, driver_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO Trips (departure, destination, departure_date, price, status, bus_id, driver_id)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    ''', (departure, destination, departure_date, price, status, bus_id, driver_id))
+    conn.commit()
+    conn.close()
+
+
+# دریافت تمام سفر ها
+def get_all_trips():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Trips')
+    trips = cursor.fetchall()
+    conn.close()
+    return trips
+
+
+# درسافت سفرخاص براساس ایدی کاربر
+def get_trip_by_id(trip_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM Trips WHERE trip_id = ?', (trip_id,))
+    trip = cursor.fetchone()
+    conn.close()
+    return trip
+
+
+# مدیدیت اتوبوس ها
+# اقزودن اتوبوس
+def add_bus(bus_number, seat_count, model, manufacturer, year, license_plate):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+        INSERT INTO Buses (bus_number, seat_count, model, manufacturer, year, license_plate)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', (bus_number, seat_count, model, manufacturer, year, license_plate))
+    conn.commit()
+    conn.close()
+
+
+# دریافت تمام اتوبوس ها
 def get_all_buses():
     conn = connect_db()
     cursor = conn.cursor()
@@ -85,8 +95,9 @@ def get_all_buses():
     conn.close()
     return buses
 
-#مدیریت رانندگان
-#اقزودن راننده
+
+# مدیریت رانندگان
+# اقزودن راننده
 def add_driver(name, license_number, birth_date, address, hiring_date, experience_years):
     conn = connect_db()
     cursor = conn.cursor()
@@ -97,7 +108,8 @@ def add_driver(name, license_number, birth_date, address, hiring_date, experienc
     conn.commit()
     conn.close()
 
-#درطافت تمام رانندگان
+
+# درطافت تمام رانندگان
 def get_all_drivers():
     conn = connect_db()
     cursor = conn.cursor()
@@ -106,7 +118,8 @@ def get_all_drivers():
     conn.close()
     return drivers
 
-#مدیریت رزرو بلیط اقزودن بلیط جدید
+
+# مدیریت رزرو بلیط اقزودن بلیط جدید
 def add_reservation(user_id, trip_id, seat_number, payment_method, reservation_code, notes, modified_by=None):
     conn = connect_db()
     cursor = conn.cursor()
@@ -117,6 +130,7 @@ def add_reservation(user_id, trip_id, seat_number, payment_method, reservation_c
     conn.commit()
     conn.close()
 
+
 def get_all_reservations():
     conn = connect_db()
     cursor = conn.cursor()
@@ -125,7 +139,8 @@ def get_all_reservations():
     conn.close()
     return reservations
 
-#دریافت رزرو خاص بر اساس ticket_id
+
+# دریافت رزرو خاص بر اساس ticket_id
 
 def get_reservation_by_ticket(ticket_id):
     conn = connect_db()
@@ -135,7 +150,8 @@ def get_reservation_by_ticket(ticket_id):
     conn.close()
     return reservation
 
-#ویرایش رزرو بلیط
+
+# ویرایش رزرو بلیط
 
 def update_reservation(ticket_id, seat_number, payment_method, notes, modified_by):
     conn = connect_db()
@@ -148,7 +164,8 @@ def update_reservation(ticket_id, seat_number, payment_method, notes, modified_b
     conn.commit()
     conn.close()
 
-#حذف رزرو بلیط
+
+# حذف رزرو بلیط
 
 def delete_reservation(ticket_id):
     conn = connect_db()
@@ -157,9 +174,11 @@ def delete_reservation(ticket_id):
     conn.commit()
     conn.close()
 
-7. #مدیریت# تراکنش‌ها (Transactions)
 
-#افزودن #تراکنش
+7.  # مدیریت# تراکنش‌ها (Transactions)
+
+
+# افزودن #تراکنش
 
 def add_transaction(ticket_id, amount, payment_method, transaction_code, status, payment_gateway, remarks):
     conn = connect_db()
@@ -171,7 +190,8 @@ def add_transaction(ticket_id, amount, payment_method, transaction_code, status,
     conn.commit()
     conn.close()
 
-#دریافت تراکنش خاص بر اساس transaction_id
+
+# دریافت تراکنش خاص بر اساس transaction_id
 
 def get_transaction_by_id(transaction_id):
     conn = connect_db()
@@ -181,9 +201,11 @@ def get_transaction_by_id(transaction_id):
     conn.close()
     return transaction
 
-8. #گزارش‌ها و آمار
 
-#تعداد کل سفرها
+8.  # گزارش‌ها و آمار
+
+
+# تعداد کل سفرها
 
 def get_total_trips():
     conn = connect_db()
@@ -193,7 +215,8 @@ def get_total_trips():
     conn.close()
     return total_trips
 
-#تعداد #سفرهای# لغو شده
+
+# تعداد #سفرهای# لغو شده
 
 def get_cancelled_trips():
     conn = connect_db()
@@ -203,7 +226,8 @@ def get_cancelled_trips():
     conn.close()
     return cancelled_trips
 
-#میزان# درآمد (جمع مبلغ سفرها)#
+
+# میزان# درآمد (جمع مبلغ سفرها)#
 
 def get_total_income():
     conn = connect_db()
@@ -212,4 +236,3 @@ def get_total_income():
     total_income = cursor.fetchone()[0]
     conn.close()
     return total_income
-
